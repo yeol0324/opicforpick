e4dad4d9ab741234ace07228900eb8e0653a723b
yeol0324 <73329772+yeol0324@users.noreply.github.com>
2025-09-06 23:08:58 +0900

    updated "myNotes"

--- apps/web/src/app/routes.tsx
+++ apps/web/src/app/routes.tsx
@@ -1,14 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "@pages/home";
-import AboutPage from "@pages/About";
 import { Layout } from "./Layout";
+import { MyNotes } from "@pages/myNotes";
 
 export const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
       { path: "/", element: <HomePage />, index: true },
-      { path: "/about", element: <AboutPage /> },
+      { path: "/myNotes", element: <MyNotes /> },
     ],
   },
 ]);
