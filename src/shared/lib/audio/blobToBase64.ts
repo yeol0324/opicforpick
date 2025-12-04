247b8e661bd1ea96357e99efa2df6f72cb1444c8
yeol0324 <73329772+yeol0324@users.noreply.github.com>
2025-12-04 11:21:35 +0900

    feat: convert blob to base64

---
+++ src/shared/lib/audio/blobToBase64.ts
@@ -0,0 +1,12 @@
+export function blobToBase64(blob: Blob): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const dataUrl = reader.result as string;
+      const base64 = dataUrl.split(",")[1];
+      resolve(base64);
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(blob);
+  });
+}
